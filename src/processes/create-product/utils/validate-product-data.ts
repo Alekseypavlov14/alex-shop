da06197b201f0ce88f33f3d4d7c06331b128f936
Alekseypavlov14 <aleshapavlov9@gmail.com>
2023-11-02 10:53:47 +0200

    added images maximum amount validation

--- src/processes/create-product/utils/validate-product-data.ts
+++ src/processes/create-product/utils/validate-product-data.ts
@@ -1,5 +1,13 @@
-import { PRODUCT_CATEGORY_ID_MINIMAL_LENGTH, PRODUCT_DESCRIPTION_MINIMAL_NAME, PRODUCT_MAXIMUM_KEYWORDS_AMOUNT, PRODUCT_MINIMAL_IMAGES_AMOUNT, PRODUCT_MINIMAL_PRICE, PRODUCT_NAME_MINIMAL_LENGTH } from "@/shared/constants/products"
 import { ProductData } from "../types/product-data"
+import { 
+  PRODUCT_CATEGORY_ID_MINIMAL_LENGTH, 
+  PRODUCT_DESCRIPTION_MINIMAL_NAME, 
+  PRODUCT_MAXIMUM_IMAGES_AMOUNT, 
+  PRODUCT_MAXIMUM_KEYWORDS_AMOUNT, 
+  PRODUCT_MINIMAL_IMAGES_AMOUNT, 
+  PRODUCT_MINIMAL_PRICE, 
+  PRODUCT_NAME_MINIMAL_LENGTH 
+} from "@/shared/constants/products"
 
 export function validateProductData(productData: ProductData) {
   return (
@@ -8,6 +16,7 @@ export function validateProductData(productData: ProductData) {
     productData.price >= PRODUCT_MINIMAL_PRICE &&
     productData.categoryId.length >= PRODUCT_CATEGORY_ID_MINIMAL_LENGTH &&
     productData.images.length >= PRODUCT_MINIMAL_IMAGES_AMOUNT &&
+    productData.images.length <= PRODUCT_MAXIMUM_IMAGES_AMOUNT &&
     productData.keywords.length <= PRODUCT_MAXIMUM_KEYWORDS_AMOUNT
   )
 }
